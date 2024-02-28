6120026961f038a53acee164bffded2447b1f8e2
Mikhail Nikulin <nklnmish@gmail.com>
2024-02-28 11:13:46 -0500

    fixed performance issues, added speed selection slider

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ function setup() {
     createCanvas(1000, 800);
     slider = createSlider(1, 25, 1);
     slider.parent('slider-container')
+
+    slider2 = createSlider(1,5,1)
+    slider2.parent('slider-speed')
 }
 
 function draw() {
@@ -24,10 +27,8 @@ function draw() {
 
     let x = 0;
     let y = 0;
-    let formula = "";
-    let formula2 = `\\[\\Large \\sum_{i=0}^{n} \\frac{4 \\cdot \\sin((2i + 1) \\cdot x)}{(2i + 1) \\pi}\\]`;
-    document.getElementById('math-formula2').innerHTML = formula2;
-    MathJax.typeset();
+    //let formula = "";
+    
 
 
     for (let i = 0; i < slider.value(); i++) {
@@ -35,8 +36,8 @@ function draw() {
         let prevY = y;
         n = i * 2 + 1;
 
-        formula += `\\(\\Huge{\\frac{4 \\cdot \\sin(${n} \\cdot x)}{${n} \\pi}}\\)` + ` + `; // LaTeX format
-        
+        //formula += `\\(\\Huge{\\frac{4 \\cdot \\sin(${n} \\cdot x)}{${n} \\pi}}\\)` + ` + `; // LaTeX format
+
 
         let coefficient = 55 * (10 / (n * PI));
 
@@ -54,6 +55,7 @@ function draw() {
         }
 
 
+
         line(prevX, prevY, x, y);
 
         stroke(255);
@@ -61,16 +63,17 @@ function draw() {
         ellipse(prevX, prevY, coefficient * 2)
 
     }
-   
-    
+
+
     fill(255, 0, 0); // Set fill color to red
-stroke(255, 10, 10); // Set stroke color to a slightly different red, if you want the border to be visible
-ellipse(x, y, 10, 10); // Draw solid ellipse
-    
-    
+    stroke(255, 10, 10); // Set stroke color to a slightly different red, if you want the border to be visible
+    ellipse(x, y, 10, 10); // Draw solid ellipse
+
+
 
 
     arr.unshift(y);
+    //console.log(arr.length)
 
 
     translate(300, 0)
@@ -89,8 +92,32 @@ ellipse(x, y, 10, 10); // Draw solid ellipse
     endShape();
 
 
-    time += 0.03;
-    document.getElementById('math-formula').innerHTML = formula;
-    MathJax.typeset(); // Ask MathJax to render the formula
+    time += slider2.value()/100;
+    // document.getElementById('math-formula').innerHTML = formula;
+    // MathJax.typeset(); // Ask MathJax to render the formula
 
 }
+
+
+window.addEventListener("load", function () {
+    let formula2 = `\\[\\Large \\sum_{i=0}^{n} \\frac{4 \\cdot \\sin((2i + 1) \\cdot x)}{(2i + 1) \\pi}\\]`;
+    document.getElementById('math-formula2').innerHTML = formula2;
+    MathJax.typeset();
+
+
+    let formula = `\\(\\Huge{\\frac{4 \\cdot \\sin(1 \\cdot x)}{1 \\pi}}\\)` + ` + `;
+    let sliderElem = document.querySelector("#slider-container input")
+    document.getElementById('math-formula').innerHTML = formula;
+    MathJax.typeset()
+    
+    sliderElem.addEventListener("change", function () {
+        console.log(sliderElem.value)
+        for(let i = 2; i < sliderElem.value; i++){
+            formula += `\\(\\Huge{\\frac{4 \\cdot \\sin(${i} \\cdot x)}{${i} \\pi}}\\)` + ` + `; // LaTeX format
+        }
+        document.getElementById('math-formula').innerHTML = formula;
+        MathJax.typeset(); // Ask MathJax to render the formula
+
+
+    })
+})
